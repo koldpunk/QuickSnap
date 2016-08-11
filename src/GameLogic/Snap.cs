19c6a2156ca1ef5e50ecaac790d38632e8e30c14
Alex Le <phuong_2202@hotmail.com>
2016-08-11 10:10:09 +1000

    add sound..?

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -148,6 +148,7 @@ namespace CardGames.GameLogic
 			{
 				_score[player]++;
 				//TODO: consider playing a sound here...
+				SwinGame.PlaySoundEffect ("Slap");
 			}
 			// added part
 			else if ( player >= 0 && player < _score.Length)
